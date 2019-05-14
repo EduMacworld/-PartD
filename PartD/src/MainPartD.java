e307f71562166d5c17d0240a0d95c3c25f17395e
EduMacworld <stuart@eduworldz.com>
2019-05-15 09:54:14 +1200

    Added best day of the week

--- PartD/src/MainPartD.java
+++ PartD/src/MainPartD.java
@@ -1,11 +1,11 @@
 
 public class MainPartD {
-
+// assignment 2 Part D. Adding lines and committing changes
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Stuart");
 		System.out.println("My favourite colour is blue");
 		System.out.println("My favourite mode of transportation is walking");
-
+		System.out.println("The best day of the week is Sunday");
 	}
 
 }
